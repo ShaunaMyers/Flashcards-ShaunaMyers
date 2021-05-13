7cbb99cfcbd0952b35c74896f8766c3e8acf749f
Shauna Myers <shaunamyers26@gmail.com>
2021-05-13 12:26:53 -0400

    Change test functions to fat arrows

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,11 +4,11 @@ const expect = chai.expect;
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
-describe('Deck', function() {
+describe('Deck', () => {
 
   let card1, card2, card3, deck;
 
-  beforeEach(function() {
+  beforeEach(() => {
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
@@ -16,19 +16,19 @@ describe('Deck', function() {
     deck = new Deck([card1, card2, card3]);
   })
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Deck).to.be.a('function');
   });
 
-  it('should be an instance of Deck', function() {
+  it('should be an instance of Deck', () => {
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  it('should store cards', function() {
+  it('should store cards', () => {
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
-  it('should return the number of cards in the Deck', function() {
+  it('should return the number of cards in the Deck', () => {
     expect(deck.countCards()).to.equal(3);
   });
 });
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,58 +4,44 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
-describe('Turn', function() {
+describe('Turn', () => {
 
   let card, turn;
 
-  beforeEach(function() {
+  beforeEach(() => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     turn = new Turn('guess', card);
   })
 
-  it('should be a function', function() {
-    const turn = new Turn();
+  it('should be a function', () => {
     expect(Turn).to.be.a('function');
   });
 
-  it('should be an instance of Turn', function() {
-    const turn = new Turn();
+  it('should be an instance of Turn', () => {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it('should store a user\'s guess', function() {
-    const card = new Card();
-    const turn = new Turn('guess', card);
+  it('should store a user\'s guess', () => {
     expect(turn.guess).to.equal('guess');
   });
 
-  it('should store the current card in play', function() {
-    const card = new Card();
-    const turn = new Turn('guess', card);
+  it('should store the current card in play', () => {
     expect(turn.card).to.equal(card);
   });
 
-  it('should return the user\'s guess', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('guess', card);
+  it('should return the user\'s guess', () => {
     expect(turn.returnGuess()).to.equal('guess');
   });
 
-  it('should return the card in play', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('guess', card);
+  it('should return the card in play', () => {
     expect(turn.returnCard()).to.equal(card);
   });
 
-  it('should evaluate if the user\'s guess is correct', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('guess', card);
+  it('should evaluate if the user\'s guess is correct', () => {
     expect(turn.evaluateGuess()).to.equal(false);
   });
 
-  it('should let the user know if their guess is correct', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('guess', card);
+  it('should let the user know if their guess is correct', () => {
     expect(turn.giveFeedback()).to.equal('Incorrect!');
   })
 });
